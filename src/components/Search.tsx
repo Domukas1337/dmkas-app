3ea80d410fbd5f5093844d805621d81886da17da
Domas <domukas1337@gmail.com>
2024-11-29 16:12:42 +0200

    changed search component color

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -10,11 +10,11 @@ export default function Search() {
 
     return (
         <div className="dark:text-white">
-            <div className={`flex flex-row justify-center items-center border ${searchValue ? "bg-red-300 dark:bg-red-700" : "bg-black"} border-gray-400 rounded-full transition-all duration-200`}>
+            <div className={`flex flex-row justify-center items-center border ${searchValue ? "bg-red-300 dark:bg-red-700" : "bg-slate-800"} border-gray-400 rounded-full transition-all duration-200`}>
                 <input 
                     type="text" 
                     placeholder="Search..." 
-                    className="px-4 py-2 sm:w-96 w-48 rounded-full z-10 outline-none shadow-md dark:bg-black dark:text-white"
+                    className="px-4 py-2 sm:w-96 w-48 rounded-full z-10 outline-none shadow-md dark:bg-slate-800 dark:text-white"
                     value={searchValue}
                     onChange={handleInputChange}
                     onKeyDown={(event) => {
@@ -27,7 +27,7 @@ export default function Search() {
                 />
                     <Link
                         type="button"
-                        className={`${searchValue ? "translate-x-0 opacity-100 bg-red-300 dark:bg-red-700 hover:px-5" : "-translate-x-6 opacity-0 bg-black cursor-default"} px-4 py-2 rounded-e-full z-0 transition-all duration-200 outline-none`}
+                        className={`${searchValue ? "translate-x-0 opacity-100 bg-red-300 dark:bg-red-700 hover:px-5" : "-translate-x-6 opacity-0 cursor-default"} px-4 py-2 rounded-e-full z-0 transition-all duration-200 outline-none`}
                         to={searchValue ? `/anime?q=${searchValue}` : ""}
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="scale-150" viewBox="0 0 16 16">
