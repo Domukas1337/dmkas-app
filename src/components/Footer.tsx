f25803bf894d3e22d324a8cbd2b1b20e7be7789f
Domas <domukas1337@gmail.com>
2025-01-20 18:29:52 +0200

    footer now leads to github page + changed sorting wording

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
   return (
     <footer className="flex justify-center items-center mx-2 bg-white mb-2 dark:bg-black shadow-lg">
-      <h1 className="text-white">&copy; {new Date().getFullYear()} DMKAS</h1>
+      <Link to={"https://github.com/Domukas1337"}>
+        <h1 className="text-white hover:text-red-500 transition-colors duration-200">
+          &copy; {new Date().getFullYear()} DMKAS
+        </h1>
+      </Link>
     </footer>
   );
 }
